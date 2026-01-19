fd9d6e634e2179e193d0a5ba8fb31f6b97ab2a0a
Nikhil <112792491+Nikohoma@users.noreply.github.com>
2026-01-19 10:49:08 +0530

    Question2- Multiplication Table Added

---
+++ MultiplicationTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MuktiplicationTable
+{
+    /// <summary>
+    /// Class having method to calculate Multiplication table
+    /// </summary>
+    public class MultiplicationTable
+    {
+        /// <summary>
+        /// Method to calculate Multiplication Table
+        /// </summary>
+        public int[] multiplicationTable(int min, int max)
+        {
+            int[] result = new int[max];
+            for (int i = 1; i<= max; i++)
+            {
+                int res = min * i;
+                result[i-1] = res;
+            }
+            
+            return result;
+
+        }
+    }
+
+    /// <summary>
+    /// Main Class 
+    /// </summary>
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MultiplicationTable m = new MultiplicationTable(); // Instance of the class incorporating the method
+            foreach (var l in m.multiplicationTable(3, 5)) { Console.Write(l +" "); } // To Print Values stored in the result list
+        }
+    }
+}
